70a8f5e2935b2a4df51f7be0d6f69a0b4c9cdd72
周权 <zhouquan02@bilibili.com>
2017-05-23 18:42:19 +0800

    modifier port

--- broadcast/main.cpp
+++ broadcast/main.cpp
@@ -2,9 +2,12 @@
 #include "broadcast.hpp"
 #include <string>
 #include <iostream>
+#include <unistd.h>
 using namespace bd_so;
 int main() {
 #ifdef SENDER
+	usleep(5*1000*1000);
+	printf("sender\n");
 	bd_so::BroadcastCenter center(true);
 	center.startSend("hello");
 #else 
