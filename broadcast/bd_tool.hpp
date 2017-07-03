20e1fa5df8b64d9612eab96c43132a423c796463
周权 <zhouquan02@bilibili.com>
2017-07-03 18:30:22 +0800

    mmap

--- broadcast/bd_tool.hpp
+++ broadcast/bd_tool.hpp
@@ -2,4 +2,5 @@
 #define __BD_TOOL__
 #include <string>
 std::string boardcast_addr(char *ip);
+#define dk_perror(fmt) printf("ERROR:%s: function: %s line:%d\n",__FILE__,__func__,__LINE__);perror(fmt)
 #endif
--- broadcast/broadcast.cpp
+++ broadcast/broadcast.cpp
@@ -6,6 +6,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <pthread.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 const int PORT = 7774;
 std::string local_ip;
 void bd_so::BroadcastCenter::startSend(std::string msg,std::string &server_ip) {
@@ -41,18 +43,28 @@ void bd_so::BroadcastCenter::init_addr() {
 	user_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
 	socket_fd = socket(AF_INET,SOCK_DGRAM,0);
 	if(-1 == socket_fd) {
-		perror("socket init failed");
+		dk_perror("socket init failed");
 		exit(1);
 	}
 	setsockopt(socket_fd,SOL_SOCKET,SO_BROADCAST,&so_broadcast,sizeof(so_broadcast));
 	if(!this->is_sender) {
 		if((bind(socket_fd,(struct sockaddr *)&user_addr,sizeof(struct sockaddr)))==-1){
-			perror("bind failed");
+			dk_perror("bind failed");
 			exit(1);
 		}
 	}
 }
 
+void* build_map_ptr() {		
+	const char* home_file_path = getenv("HOME");
+	const char *filepath = "/device_ip_pair";	
+	char *map_file_path= (char *)malloc(1024);
+	strcat(map_file_path,home_file_path);
+	strcat(map_file_path,filepath);
+	int map_fd = open(map_file_path,O_RDWR|O_CREAT);
+	void *ptr = mmap(NULL,4*1024*25,PROT_WRITE,MAP_SHARED,map_fd,0);
+	return ptr;
+}
 
 void bd_so::startReceiving(void *) {
 	bd_so::BroadcastCenter *center = new bd_so::BroadcastCenter(false);
@@ -63,17 +75,23 @@ LISTEN:
 	strcpy(center->my_ip,inet_ntoa(center->user_addr.sin_addr));
 	bzero(center->buf,MAXDATASIZE);
 	strcpy(center->buf,local_ip.c_str());
+
 	sendto(center->socket_fd,center->buf,strlen(center->buf),0,(struct sockaddr *)&center->user_addr,sizeof(sockaddr_in));
 	goto LISTEN;
 }
 
 extern void dk_deamonInit();
 void bd_so::BroadcastCenter::start_listen_thread(void) {
+	void *ptr = build_map_ptr();
+	if(ptr == MAP_FAILED) {
+		dk_perror("map file error");
+	}
+
 	dk_deamonInit();
 	pthread_t listen_th;
 	void (*func)(void *) = bd_so::startReceiving;
 	if(-1 == pthread_create(&listen_th,NULL,(void* (*)(void *))func,(void *)NULL)) {
-		perror("listen thread init failed");
+		dk_perror("listen thread init failed");
 		exit(1);
 	}
 	int *result = new int;
